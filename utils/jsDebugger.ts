44de25d7bdd83dfc074f770f51f486c479455089
ClausWong <726787295@qq.com>
2025-07-16 09:25:49 +0800

    优化

--- utils/jsDebugger.ts
+++ utils/jsDebugger.ts
@@ -2,24 +2,28 @@
  * processor断点自定义工具
  * 1.传参支持string、array
  * 2.函数可移植性
- * 3.打印变化文件路径
- * 4.断点
+ * 3.断点
+ * 4.matchMode 精准、模糊匹配
+ * 5.二次操作查看processor包装顺序
  *
  * Ts转Js网站: https://www.typescriptlang.org/play?#code
  * 注意：放在类中去掉function关键字了
  */
-function superDebug(fieldName: string) {
+type MatchMode = 'fuzzy' | 'explict'
+
+function superDebug(fieldName: string, matchMode?: MatchMode) {
     const debuggerKeys = window['debuggerKeys']
+    fieldName = fieldName.split('.').pop()
     if (debuggerKeys) {
         if (typeof debuggerKeys === 'string') {
-            if(isHitTarget(debuggerKeys, fieldName)) {
+            if(isHitTarget(debuggerKeys, fieldName, matchMode)) {
                 console.log('debuggerKeys', fieldName)
                 debugger
             }
         } else if (Array.isArray(debuggerKeys)) {
             debuggerKeys.forEach(key => {
                 console.log('debuggerKeys', fieldName)
-                if(isHitTarget(key, fieldName)){
+                if(isHitTarget(key, fieldName, matchMode)){
                     console.log('debuggerKeys', fieldName)
                     debugger
                     return
@@ -28,34 +32,46 @@ function superDebug(fieldName: string) {
         }
     }
 
-    function isHitTarget(key: string, target: string): boolean {
-        return new RegExp(key).test(target);
+    function isHitTarget(key: string, target: string, matchMode?: MatchMode): boolean {
+        matchMode = matchMode || 'fuzzy' // 默认模糊匹配
+        if (matchMode === 'fuzzy') {
+            return new RegExp(key).test(target);
+        } else {
+            return key === target;
+        }
     }
 }
 
 /** 编译后
- superDebug(fieldName) {
+ superDebug(fieldName, matchMode) {
     const debuggerKeys = window['debuggerKeys'];
+    fieldName = fieldName.split('.').pop();
     if (debuggerKeys) {
-        if (typeof debuggerKeys === 'string') {
-            if (isHitTarget(debuggerKeys, fieldName)) {
-                console.log('debuggerKeys', fieldName);
-                debugger;
-            }
+      if (typeof debuggerKeys === 'string') {
+        if (isHitTarget(debuggerKeys, fieldName, matchMode)) {
+          console.log('debuggerKeys', fieldName);
+          debugger;
         }
-        else if (Array.isArray(debuggerKeys)) {
-            debuggerKeys.forEach(key => {
-                console.log('debuggerKeys', fieldName);
-                if (isHitTarget(key, fieldName)) {
-                    console.log('debuggerKeys', fieldName);
-                    debugger;
-                    return;
-                }
-            });
-        }
+      }
+      else if (Array.isArray(debuggerKeys)) {
+        debuggerKeys.forEach(key => {
+          console.log('debuggerKeys', fieldName);
+          if (isHitTarget(key, fieldName, matchMode)) {
+            console.log('debuggerKeys', fieldName);
+            debugger;
+            return;
+          }
+        });
+      }
     }
-    function isHitTarget(key, target) {
+    function isHitTarget(key, target, matchMode) {
+      matchMode = matchMode || 'fuzzy'; // 默认模糊匹配
+      if (matchMode === 'fuzzy') {
         return new RegExp(key).test(target);
+      }
+      else {
+        return key === target;
+      }
     }
-}
+  }
  */
